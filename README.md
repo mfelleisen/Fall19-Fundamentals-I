f8dd4bba578ebfbd96523c05f3d8dd0e2847eb68
Matthias Felleisen <matthias@ccs.neu.edu>
2018-08-31 21:45:15 -0400

    specify info dependencies

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Fall19-Funamentals-I
 ====================
-README text here.
+
+- scribblings ~~ documentation 
+- Resources ~~ the PNGs for the GUI and the sample MP3s for files/
+- files/ ~~ a simple client-server system for exchanging and playing MP3s
--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "Fall19-Funamentals-I")
-(define deps '("base"))
+(define deps '("racket" "video-v0-2"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/Fall19-Funamentals-I.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.1")
+(define pkg-desc "a supplementary package for Fundamentals I @ NEU, Fall 2018")
+(define version "0.2")
 (define pkg-authors '(matthias))
